7de50d0b68b2b496014758f4b45e7f30ba443793
tliber <toleliberman@gmail.com>
2014-12-23 17:40:26 -0800

    better collision detection based in space/time per movement also converted circles to gifs

--- watchout.js
+++ watchout.js
@@ -3,7 +3,9 @@ var collision = false;
 var collisionCounter = 0;
 var highScore = 0;
 var currentScore = 0;
-// start slingin' some d3 here.
+var enemyMovementSpeed = 1200;
+var enemySpaceTime = 0;//never change
+// start slin' some d3 here.
 
 var svg = d3.select("body").append("svg")
   .attr({"width" : 500,
@@ -12,7 +14,7 @@ var svg = d3.select("body").append("svg")
   'border-size': 15 + 'px',
   "border-color" :'black',
   "border-style" : "solid",
-  'background-image' : 'url(sound_of_space.jpg)'});
+  'background-image' : 'url(orangeSpace.jpg)'});
   // .append("battleField")
 
   // .attr("cy", 25)
@@ -45,7 +47,9 @@ var makeEnemies = function(amount){
     var enemy = {
       'id': i,
       'x': coordinates[i][0],
-      'y': coordinates[i][1]
+      'y': coordinates[i][1],
+      'oldX' : coordinates[i][0],
+      'oldY' : coordinates[i][1],
     };
     enemies.push(enemy)
   // console.log(enemies);
@@ -56,87 +60,100 @@ var enemies = makeEnemies(25);
 var Enemies = d3.select("svg").selectAll('div')
   .data(enemies)
   .enter()
-  .append("circle")
-  .attr("cx", function(d){return d.x})
-  .attr("cy", function(d){return d.y})
-  .attr('id', function(d){return d.id})
-  .attr("r", 10).style("fill", "black");
+  .append("image")
+  .attr('class','enemy')
+  .attr('xlink:href','succubus.gif')
+  .attr("x", function(d){return d.x})
+  .attr("y", function(d){return d.y})
+  .attr('height', 50 + 'px')
+  .attr('width',50 + 'px')
 
 
 var moveEnemies = function(amount){
   // console.log(enemies)
   newXYS = makeXY(25);
   for (var i = 0;i < enemies.length;i++){
+    // console.log('in move' + enemies[i].x)
+    enemies[i]['oldX'] = enemies[i].x; 
+    enemies[i]['oldY'] = enemies[i].x; 
     enemies[i].x = newXYS[i][0];
     enemies[i].y = newXYS[i][1];
   }
-  // console.log(enemies)
-  d3.selectAll('circle')
+
+
+  d3.selectAll('image')
   .data(enemies)
   .transition()
-  .duration(500)
-  .attr("cx",function(d){return d.x})
-  .attr("cy", function(d){return d.y})
-  // .attr("r", 10).style("fill", "purple");
-  // console.log(l);
-  // .data(makeXY(amount))
-  // .enter()
-  
-  // .enter()
+  .duration(enemyMovementSpeed)
+  .attr("x",function(d){return d.x})
+  .attr("y", function(d){return d.y})
+
 };
-// addEnemies(25);  
-// var motionOfEnemies = function(){
+
+
 var HeroProps = [{"x" : 200, "y" : 200,}]
-  // 'border-size': '15px',
-  // "border-color" :'black',
-  // "border-style" : "solid"}
-
-
-
 var hero = svg
-  .append('circle')
+  // .append('circle')
   .data(HeroProps)
-  .attr("cx", function(d){return d.x})
-  .attr("cy", function(d){return d.y})
-  .attr("r", 20)
+  // .attr("cx", function(d){return d.x})
+  // .attr("cy", function(d){return d.y})
+  // .attr("r", 20)
+  // .style("fill", "transparent")
+  .append('image')
   .attr('class' , 'hero')
+  .attr('xlink:href','resizedLama2.gif')
+  .attr("x", function(d){return d.x})
+  .attr("y", function(d){return d.y})
+  .attr('height', 80 + 'px')
+  .attr('width',80 + 'px')
   // .attr("onmousedown","selectElement(evt)")
-  .style("fill", "orange")
+  // .style("fill", "url(resizedLama.gif)")
 
 
 var drag = d3.behavior.drag()
   .on("drag", function(d,i) {
-    hero.attr('cx', function(d) {
+    hero.attr('x', function(d) {
       d.x = d.x + d3.event.dx
       
-      return d.x;
+      return d.x;   
     })
-      .attr('cy', function(d) {
+      .attr('y', function(d) {
         d.y = d.y + d3.event.dy
         return d.y;
       })
   })
 hero.call(drag);
 
-// console.log(hero);
-// console.log(hero)
+
 var collisionCheck = function(){
-  // console.log('running c check')
-  var heroX =  d3.select(".hero").attr("cx");
-  var heroY =  d3.select(".hero").attr("cy");
+  if (enemySpaceTime < enemyMovementSpeed + 1){
+    enemySpaceTime = enemySpaceTime + 5;
+  } else{
+    enemySpaceTime = 0;
+  }
+  var wayThere = enemySpaceTime/enemyMovementSpeed;
+  
+  // var transitionItemProps = d3.selectAll('circle');
+
+
+  var heroX =  d3.select(".hero").attr("x");
+  var heroY =  d3.select(".hero").attr("y");
   // console.log(heroX)
   var thresh = 50;
+  console.log(enemies.length);
   for (var i = 0;i < enemies.length;i++){
-    // console.log((Math.abs(heroX - enemies[i].x) + Math.abs(heroY - enemies[i].y)));
-    // console.log((Math.abs(heroX - enemies[i].x) + 'assumed X dif'));
-    // var howClose = (Math.abs(heroX - enemies[i].x) + Math.abs(heroY - enemies[i].y))
-    // console.log(howClose + ' this close' + enemies[i].x + 'enemy x')
-    // console.log(howClose + ' this close' + enemies[i].y + 'enemy y')
-
+    console.log(enemies[i])
+    var oldX = enemies[i]['oldX'];
+    var oldY = enemies[i]['oldY'];
+    var currentEnemyX = oldX - ((oldX - enemies[i].x) * wayThere);
+    var currentEnemyY = oldY - ((oldY - enemies[i].y) * wayThere);
+    var endPoint = (Math.abs(heroX - currentEnemyX) + Math.abs(heroY - currentEnemyY));
     // debugger;
-    // console.log(i);
-    if (thresh > (Math.abs(heroX - enemies[i].x) + Math.abs(heroY - enemies[i].y))){
+
+
+    if (thresh > endPoint){
       // console.log('collision');
+
       
       collisionCounter++;
       return true;
@@ -146,77 +163,20 @@ var collisionCheck = function(){
     return false;
   // console.log(enemies)
 } 
-// debugger;
-// collisionCheck(hero);
-setInterval(moveEnemies.bind(null,25), 500);
+
+setInterval(moveEnemies.bind(null,25), 1200);
 setInterval(function(){
   var checker = collisionCheck();
   if (checker === true){
-    // console.log('hit')
     currentScore = 0;
     d3.select('.high span').text(highScore)
     d3.select('.collisions span').text(collisionCounter);
   }
   else
-    // collisionCounter++;
     currentScore++;
     if(currentScore > highScore){
       highScore = currentScore;
+      // debugger;
     } 
     d3.select('.current span').text(currentScore);
-  }, 5);// setInterval(function(){
-
-// },100)
- // function selectElement(evt) {
-    //pointer to mouse
-    // selectedElement = evt.target;
-
-    //pointer to mouse location
-    // var currentX = evt.clientX;
-    // var currentY = evt.clientY;
-    // console.log(selectedElement);
-    // console.log(currentX)
-    // console.log(currentY)
-    // d3.select(selectedElement)
-    // .attr('cx', currentX)
-    // .attr('cy', currentY)
-    // .data([currentX, currentY])
-
-    // currentMatrix = selectedElement.getAttributeNS(null, "transform").slice(7,-1).split(' ');
-     
-
-    // for(var i=0; i<currentMatrix.length; i++) {
-
-      // currentMatrix[i] = parseFloat(currentMatrix[i]);
-
-      // }
-
- 
-
-    // selectedElement.setAttributeNS(null, "onmousemove", "moveElement(evt)");
-
-  
-
-
-
-
-
-
-
-
-  // .behavior.drag()
-  // .on("drag", dragmove);;
-  // .attr('width' : 60 + 'px',
-  // "height" : '60' + 'px',
-  // 'border-size': '15' + 'px',
-  // "border-color" :'black',
-  // "border-style" : "solid"}
-
-  // moveEnemies(25);
-  // console.log(Neo);
-// motionOfEnemies();
-// var drag = d3.behavior.drag()  
-             // .on('dragstart', function() { circle.style('fill', 'red'); })
-             // .on('drag', function() { circle.attr('cx', d3.event.x)
-                                            // .attr('cy', d3.event.y); })
-             // .on('dragend', function() { circle.style('fill', 'black'); });
+  }, 5);
